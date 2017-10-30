b7fa275757eddf05abeccee0e439bb793b94a025
kael ho <helong9291@sina.com>
2017-10-30 11:36:24 +0800

    Update shaizi.py

--- pyfiles/shaizi.py
+++ pyfiles/shaizi.py
@@ -1,36 +1,57 @@
 #-*- coding: utf-8 -*-
-
-import matplotlib.pyplot as plt
-import numpy as np
-import random
-
-x = []
-y = []
-print 'Please input your dice number:'
-index = int(input())-1
-print index
-shaizi = []
-#0,1,2
-for i in range(5):
-    shaizi.append([])
-    #6,36,216
-    for j in range(6**(i+1)):
-	count = (j/(6**i)+1) + (j%6+1)*i
-	shaizi[i].append(count)
-for i in range(len(shaizi)):
-    print shaizi[i]
-for i in range(10000):
-    rand = shaizi[index][random.randint(0,len(shaizi[index])-1)]
-    if rand not in x:
-        x.append(rand)
-        y.append(1)
-    else:
-	y[x.index(rand)]+=1
-plt.scatter([xx/(1.0+index) for xx in x],y, color='k', s=25, marker="o")
-plt.ylim(1,10000/((index+1)**2))
-plt.xlabel(u'色子数值')
-plt.ylabel(u'数值对应个数')
-plt.title('Shaizi Plot')
-plt.legend()
-
-plt.show()
+  2 
+  3 import matplotlib.pyplot as plt
+  4 import numpy as np
+  5 import random
+  6 
+  7 '''
+  8 生成二维数组[[],[],[],[],[],[]]
+  9 1.参数为1：[[1],[2],[3],[4],[5],[6]]
+ 10 2.参数为2：[[1+1,1+2,1+3,1+4,1+5,1+6]......[6+1,6+2,6+3,6+4,6+5,6+6]]
+ 11 3.参数为3：[[1+1+1,1+1+2,1+1+3,1+1+4,1+1+5,1+1+6,  1+2+1,1+2+2,1+2+3,1+2+4,1+2+5,1+2+6,]......]
+ 12 '''
+ 13 def makeArray(dimension):
+ 14     array=[[],[],[],[],[],[]]
+ 15     for i in range(dimension):
+ 16         if i==0:
+ 17             for j in range(6):
+ 18                 array[j].append(j+1)
+ 19         else:
+ 20             for j in range(len(array)):
+ 21                 temp = array[j]
+ 22                 array[j]=[]
+ 23                 for k in range(6):
+ 24                     for m in range(len(temp)):
+ 25                         array[j].append(temp[m]+k+1)
+ 26     target = []
+ 27     for i in range(len(array)):
+ 28         target+=array[i]
+ 29     return target
+ 30 
+ 31 #print 'Please input your dice number:'
+ 32 #index = int(input())
+ 33 index = [1,2,3,6,8,10]
+ 34 shaizi = []
+ 35 for i in range(len(index)):
+ 36     shaizi.append(makeArray(index[i]))
+ 37 count = 100000
+ 38 for z in range(len(index)):
+ 39     x = []
+ 40     y = []
+ 41     for i in range(count):
+ 42         rand = shaizi[z][random.randint(0,len(shaizi[z])-1)]
+ 43         if rand not in x:
+ 44             x.append(rand)
+ 45             y.append(1)
+ 46         else:
+ 47             y[x.index(rand)]+=1
+ 48     plt.figure(z+1)
+ 49     plt.scatter([xx/(1.0*(index[z])) for xx in x],y, color='k', s=25, marker="o")
+ 50     plt.xlim(0,7)
+ 51     plt.ylim(1,count/5)
+ 52     plt.xlabel(u'number')
+ 53     plt.ylabel(u'count')
+ 54     plt.title('Dice : '+str(index[z]))
+ 55     plt.legend()
+ 56 
+ 57 plt.show()
