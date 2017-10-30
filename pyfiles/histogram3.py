53df4e4ff831293375df83484a08f5f5c8b4dd0e
kael ho <helong9291@sina.com>
2017-10-31 00:17:56 +0800

    Add files via upload

---
+++ pyfiles/histogram3.py
@@ -0,0 +1,35 @@
+#-*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+'''
+实现绘制横坐标为年龄，纵坐标为对应年龄范围的人数的直方图
+'''
+
+'''
+population_ages = [10,15,20,25,15,20,25,30,20,25,30,35,25,30,35,40]
+population_ages = []
+for i in range(10000):
+    population_ages.append(random.randint(1,110))
+'''
+population_ages = np.loadtxt('facebook.csv', delimiter=',', unpack=True)
+population_ages = sorted(population_ages)
+print population_ages[0]
+print population_ages[-1]
+
+print 'please input step:'
+step = int(input())
+
+bins = []
+for i in range(0,int(population_ages[-1]/10*10+10),step):
+    bins.append(i)
+
+plt.hist(population_ages, bins, histtype='bar')
+plt.xlabel('age')
+plt.ylabel('number')
+plt.title('histogram for age--count')
+plt.legend()
+
+plt.show()
