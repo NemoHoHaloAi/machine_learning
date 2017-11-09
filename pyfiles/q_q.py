36a7dd51bfdcb16241a300ab30f14a6a87e63124
helong <nevermore.he@aispeech.com>
2017-11-09 11:29:08 +0800

    fix q_q.py and add q_q image to project 1

--- pyfiles/q_q.py
+++ pyfiles/q_q.py
@@ -9,28 +9,38 @@ import random
 两组数据各选出99个点将每组数据分为100分，
 再将这99个点分别作为横纵坐标绘制出来
 '''
+normal = [-1.28,-0.84,-0.52,-0.25,0,0.25,0.52,0.84,1.28]
 
 array_a,array_b=np.loadtxt('q_q.txt', delimiter=',', unpack=True)
 array_a = sorted(array_a)
 array_b = sorted(array_b)
+
 np_a = np.array((array_a))
 np_b = np.array((array_b))
+np_normal = np.array((normal))
 
-x=[]
-y=[]
-#np.percentile(np_a,k)
-#计算以np_a为数据集，k%位置对应的数据点
+x_a=[]
+y_a=[]
+x_b=[]
+y_b=[]
+#计算以np_xx为数据集，i%位置对应的数据点
 for i in range(1,100):
-    x.append(np.percentile(np_a,i))
-    y.append(np.percentile(np_b,i))
+    x_a.append(np.percentile(np_normal,i))
+    y_a.append(np.percentile(np_a,i))
+    x_b.append(np.percentile(np_normal,i))
+    y_b.append(np.percentile(np_b,i))
 
-print x
-print y
-plt.scatter(x,y, color='k', s=25, marker="o")
-_max = array_a[-1] if array_a[-1]>array_b[-1] else array_b[-1]
-plt.plot([0,_max+5],[0,_max+5])
-plt.xlim(0,_max+5)
-plt.ylim(0,_max+5)
+print x_a
+print y_a
+print x_b
+print y_b
+
+plt.figure(0)
+plt.scatter(x_a,y_a, color='k', s=25, marker="o")
+plt.title('Q-Q')
+plt.legend()
+plt.figure(1)
+plt.scatter(x_b,y_b, color='b', s=25, marker="o")
 plt.title('Q-Q')
 plt.legend()
 plt.show()
