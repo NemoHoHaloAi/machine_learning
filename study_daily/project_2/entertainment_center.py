8d95dde19b134b4e13ca680c1e065171181c04a9
kael ho <helong9291@sina.com>
2017-11-15 18:07:35 +0800

    Update entertainment_center.py

--- study_daily/project_2/entertainment_center.py
+++ study_daily/project_2/entertainment_center.py
@@ -1,9 +1,9 @@
 #-*- coding: utf-8 -*-
 
-'''
+"""
 该模块主要为项目运行模块，主要负责接收用户输入，调用
 其他模块方法以及使用其他模块类，并最终生成html静态文件
-'''
+"""
 
 from media import Movie
 from fresh_tomatoes import open_movies_page
@@ -15,6 +15,12 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def user_input():
+    """
+    This function for receive user input love movie name, spite with "，", end with enter.
+    
+    Returns:
+        Return user input string.
+    """
     keys = raw_input('输入喜爱的电影，并使用中文逗号分割：\n').split('，')
     return keys
 
