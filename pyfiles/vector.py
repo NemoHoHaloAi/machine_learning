ceefb7ec189cf4fed6d21f592e8a57e133a54bc1
kael <helong9291@sina.com>
2017-12-17 12:24:28 +0800

    finish vector.py

--- pyfiles/vector.py
+++ pyfiles/vector.py
@@ -29,7 +29,31 @@ def enviroment_init():
 #     """
 #     return [[(Decimal(0) if i >= len(list1) else list1[i]), (Decimal(0) if i >= len(list2) else list2[i])] for i in range(max(len(list1),len(list2)))]
 
-def ZeroVector(func):
+def Beyond3Dimension(func):
+    """
+    三维向量装饰器，处理当向量超过三维时，抛出异常
+    """
+    def wrapper(*args, **kwargs):
+        """
+        Returns:
+            result -- func的执行结果
+            
+        Raises:
+            Exception -- 当计算中出现除0时抛出该异常
+        """
+        # TODO(Ho Loong): 暂时使用这种方法判断是否other或者self为空，后续看有没有更优雅的方法
+        self = kwargs.get('self') if kwargs.get('self') else args[0]
+        other = kwargs.get('other') if kwargs.get('other') else args[1]
+        if len(self) not in (2,3) or len(other) not in (2,3):
+            raise Exception('Vector Exception:No support beyond 3 or less 2 dimension vector to calc.')
+        if len(self) == 2: 
+            self.full(3)
+        if len(other) == 2: 
+            other.full(3)
+        return func(*args, **kwargs)
+    return wrapper
+
+def ZeroReturnTrue(func):
     """
     零向量装饰器，处理当other为零向量时直接返回True，也就不需要对零向量特殊处理
     """
@@ -41,12 +65,31 @@ def ZeroVector(func):
         # TODO(Ho Loong): 暂时使用这种方法判断是否other或者self为空，后续看有没有更优雅的方法
         self = kwargs.get('self') if kwargs.get('self') else args[0]
         other = kwargs.get('other') if kwargs.get('other') else args[1]
-        if (self and abs(sum(self.coordinates)) <= ZERO) or (other and abs(sum(other.coordinates)) <= ZERO):
+        if (self and self.is_zero()) or (other and other.is_zero()):
             return True
         return func(*args, **kwargs)
     return wrapper
 
-def ZeroDivision(func):
+def ZeroReturnZeroVector(func):
+    """
+    零向量装饰器，处理当other为零向量时直接返回零向量，也就不需要对零向量特殊处理
+    """
+    def wrapper(*args, **kwargs):
+        """
+        Returns:
+            result -- 包含一个零向量时返回True，否则返回func
+        """
+        # TODO(Ho Loong): 暂时使用这种方法判断是否other或者self为空，后续看有没有更优雅的方法
+        self = kwargs.get('self') if kwargs.get('self') else args[0]
+        other = kwargs.get('other') if kwargs.get('other') else args[1]
+        if self and self.is_zero():
+            return self
+        if other and other.is_zero():
+            return other
+        return func(*args, **kwargs)
+    return wrapper
+
+def ZeroCalc(func):
     """
     除0异常装饰器，处理function中可能发生的除0异常
     """
@@ -97,6 +140,12 @@ class Vector(object):
             coordinates -- 向量值列表
         """
     	self.coordinates = tuple([Decimal(me) for me in coordinates])
+
+    def __len__(self):
+        """
+        重载len()运算，返回coordinates长度
+        """
+        return len(self.coordinates)
 
     def __eq__(self, other):
         """
@@ -157,9 +206,37 @@ class Vector(object):
             NoneVector
         """
     	return Decimal(sum([me * him for me, him in zip(self.coordinates, other.coordinates)]))
+
+    def get_coordinates(self):
+        """
+        获取当前向量的值列表
+
+        Returns:
+            result -- 当前向量的值列表
+        """
+        return self.coordinates
+
+    def full(self, dimension):
+        """
+        用0填充当前向量使其匹配更高维度的向量
+
+        Args:
+            dimension -- 目标维度
+        """
+        if len(self) < dimension:
+            self.coordinates = tuple(list(self.coordinates) + ([0] * (dimension - len(self))))
+
+    def is_zero(self):
+        """
+        判断当前向量是不是零向量
+
+        Returns:
+            result -- True表示是零向量，False表示不是
+        """
+        return abs(sum(self.coordinates)) <= ZERO
     
     # TODO(Ho Loong): 暂时使用mul，后续看有没有更好的名字用以描述一个向量乘以标量的功能
-    def mul(self, times):
+    def scala(self, times):
         """
         实现向量与标量相乘，即向量的每一项都乘以标量
 
@@ -180,7 +257,7 @@ class Vector(object):
         """
     	return Decimal(Decimal.sqrt(sum([me**Decimal(2) for me in self.coordinates])))
 
-    @ZeroDivision
+    @ZeroCalc
     def direction(self):
         """
         计算并返回向量的方向
@@ -189,11 +266,11 @@ class Vector(object):
             result -- 标准化
 
         Decorates:
-            ZeroDivision
+            ZeroCalc
         """
-    	return self.mul(Decimal(1) / self.size())
+    	return self.scala(Decimal(1) / self.size())
     
-    @ZeroDivision
+    @ZeroCalc
     @NoneVector
     def angle_rad(self, other):
         """
@@ -206,7 +283,7 @@ class Vector(object):
             result -- 向量夹角弧度值
 
         Decorates:
-            ZeroDivision
+            ZeroCalc
             NoneVector
         """
         return Decimal(str(math.acos((self * other) / (self.size() * other.size()))))
@@ -227,7 +304,7 @@ class Vector(object):
         """
     	return Decimal(self.angle_rad(other) * (Decimal(180) / Decimal(str(math.pi))))
 
-    @ZeroVector
+    @ZeroReturnTrue
     @NoneVector
     def parallel(self, other):
         """
@@ -242,12 +319,12 @@ class Vector(object):
             result -- 是否平行的bool值
 
         Decorates:
-            ZeroVector
+            ZeroReturnTrue
             NoneVector
         """
         return (self.coordinates and other and other.coordinates and (abs(self.angle_deg(other) - Decimal(0)) <= ZERO or abs(self.angle_deg(other) - Decimal(180)) <= ZERO))
 
-    @ZeroVector
+    @ZeroReturnTrue
     @NoneVector
     def orthogonal(self, other):
         """
@@ -261,10 +338,115 @@ class Vector(object):
             result -- 是否正交的bool值
 
         Decorates:
-            ZeroVector
+            ZeroReturnTrue
             NoneVector
         """
     	return abs(self * other - Decimal(0)) <= ZERO
+
+    @ZeroCalc
+    @NoneVector
+    def parallel_vector(self, other):
+        """
+        基于basis向量计算当前向量在basis上的投影向量
+
+        Args:
+            other -- 基向量
+
+        Returns:
+            result -- self在other上的投影向量
+
+        Notes:
+            formula -- proj(self)_basis = (self.u_basis).u_basis
+
+        Decorates:
+            ZeroCalc
+            NoneVector
+        """
+        u_basis = other.direction()  # 计算basis的标准化向量，也就是它的单位向量，用direction方法即可
+        return u_basis.scala(self * (u_basis))
+
+    @ZeroCalc
+    @NoneVector
+    def orth_vector(self, other):
+        """
+        基于basis向量计算当前向量在basis上的正交向量
+
+        Args:
+            other -- 基向量
+
+        Returns:
+            result -- self在other上的正交向量
+
+        Notes:
+            formula -- self - proj(self)_basis
+
+        Decorates:
+            ZeroCalc
+            NoneVector
+        """
+        return self - self.parallel_vector(other)
+
+    @ZeroReturnZeroVector
+    @Beyond3Dimension
+    @NoneVector
+    def cross(self, other):
+        """
+        计算self与basis的叉乘结果
+
+        Args:
+            basis -- 与self计算叉乘的向量
+
+        Returns:
+            result -- 叉乘结果对应的向量
+
+        Notes:
+            formula -- self x other
+
+        Decorates:
+            ZeroCalc
+            NoneVector
+        """
+        x1,y1,z1 = self.get_coordinates();
+        x2,y2,z2 = other.get_coordinates();
+        return Vector([y1*z2-y2*z1,-(x1*z2-x2*z1),x1*y2-x2*y1])
+
+    @NoneVector
+    def parallelogram(self, other):
+        """
+        计算self与basis围成的平行四边形的面积
+
+        Args:
+            basis -- 与self计算平行四边形面积的向量
+
+        Returns:
+            result -- 平行四边形的面积
+
+        Notes:
+            formula -- (self x other).size()
+
+        Decorates:
+            NoneVector
+        """
+        return self.cross(other).size()
+
+    @NoneVector
+    def triangle(self, other):
+        """
+        计算self与basis围成的三角形的面积
+
+        Args:
+            basis -- 与self计算三角面积的向量
+
+        Returns:
+            result -- 三角的面积
+
+        Notes:
+            formula -- (self x other).size()/2
+
+        Decorates:
+            NoneVector
+        """
+        return self.cross(other).size()/Decimal(2)
 
     def __str__(self):
         """
@@ -279,8 +461,8 @@ def main():
     """
     enviroment_init()
     
-    v1 = Vector([1,2])
-    v2 = Vector([2,3])
+    v1 = Vector(['1','2'])
+    v2 = Vector(['2','3'])
     print 'v1=' + str(v1)
     print 'v2=' + str(v2)
     v3 = v1 + v2
@@ -291,7 +473,7 @@ def main():
     print 'v2-v1=' + str(v5)
     v1 += v2
     print 'v1+=v2=' + str(v1)
-    v6 = v1.mul(2)
+    v6 = v1.scala(2)
     print 'v1*2=' + str(v6)
     print 'v1-v2是否等于v2-v1=' + str((v1-v2)==(v2-v1))
     v1 = Vector([3,4])
@@ -299,26 +481,35 @@ def main():
     print 'v1 size=' + str(v1.size())
     print 'v1 direction=' + str(v1.direction())
     print '--------------No1--------------'
-    print 'No1:' + str(Vector([-0.221,7.437]).size())
-    print 'No2:' + str(Vector([8.813,-1.331,-6.247]).size())
-    print 'No3:' + str(Vector([5.581,-2.136]).direction())
-    print 'No4:' + str(Vector([1.996,3.108,-4.554]).direction())
-    print 'No5:' + str(Vector([0,0]).size())
+    print 'No1:' + str(Vector(['0.221','7.437']).size())
+    print 'No2:' + str(Vector(['8.813','-1.331','-6.247']).size())
+    print 'No3:' + str(Vector(['5.581','-2.136']).direction())
+    print 'No4:' + str(Vector(['1.996','3.108','-4.554']).direction())
+    print 'No5:' + str(Vector(['0','0']).size())
     print '--------------No2--------------'
-    print 'No1:' + str(Vector([7.887,4.138]) * Vector([-8.802,6.776]))
-    print 'No2:' + str(Vector([-5.955,-4.904,-1.874]) * Vector([-4.496,-8.755,7.103]))
-    print 'No3:' + str(Vector([3.183,-7.627]).angle_rad(Vector([-2.668,5.319])))
-    print 'No4:' + str(Vector([7.35,0.221,5.188]).angle_deg(Vector([2.751,8.259,3.985])))
-    print 'Notest:' + str(Vector([1,1,1]).angle_deg(Vector([2,2,2])))
+    print 'No1:' + str(Vector(['7.887','4.138']) * Vector(['-8.802','6.776']))
+    print 'No2:' + str(Vector(['-5.955','-4.904','-1.874']) * Vector(['-4.496','-8.755','7.103']))
+    print 'No3:' + str(Vector(['3.183','-7.627']).angle_rad(Vector(['-2.668','5.319'])))
+    print 'No4:' + str(Vector(['7.35','0.221','5.188']).angle_deg(Vector(['2.751','8.259','3.985'])))
+    print 'Notest:' + str(Vector(['1','1','1']).angle_deg(Vector(['2','2','2'])))
     print '--------------No3--------------'
-    print 'No1:' + str(Vector([-7.579,-7.88]).parallel(Vector([22.737,23.64])))
-    print 'No1:' + str(Vector([-7.579,-7.88]).orthogonal(Vector([22.737,23.64])))
-    print 'No2:' + str(Vector([-2.029,9.97,4.172]).parallel(Vector([-9.231,-6.639,-7.245])))
-    print 'No2:' + str(Vector([-2.029,9.97,4.172]).orthogonal(Vector([-9.231,-6.639,-7.245])))
+    print 'No1:' + str(Vector(['-7.579','-7.88']).parallel(Vector(['22.737','23.64'])))
+    print 'No1:' + str(Vector(['-7.579','-7.88']).orthogonal(Vector(['22.737','23.64'])))
+    print 'No2:' + str(Vector(['-2.029','9.97','4.172']).parallel(Vector(['-9.231','-6.639','-7.245'])))
+    print 'No2:' + str(Vector(['-2.029','9.97','4.172']).orthogonal(Vector(['-9.231','-6.639','-7.245'])))
     print 'No3:' + str(Vector([-2.328,-7.284,-1.214]).parallel(Vector([-1.821,1.072,-2.94])))
     print 'No3:' + str(Vector([-2.328,-7.284,-1.214]).orthogonal(Vector([-1.821,1.072,-2.94])))
     print 'No4:' + str(Vector([2.118,4.827]).parallel(Vector([0,0])))
     print 'No4:' + str(Vector([2.118,4.827]).orthogonal(Vector([0,0])))
+    print '--------------No4--------------'
+    print 'No1:' + str(Vector([3.039,1.879]).parallel_vector(Vector([0.825,2.036])))
+    print 'No2:' + str(Vector([-9.88,-3.264,-8.159]).orth_vector(Vector([-2.155,-9.353,-9.473])))
+    print 'No3:' + str(Vector([3.009,-6.172,3.692,-2.51]).parallel_vector(Vector([6.404,-9.144,2.759,8.718])))
+    print 'No4:' + str(Vector([3.009,-6.172,3.692,-2.51]).orth_vector(Vector([6.404,-9.144,2.759,8.718])))
+    print '--------------No5--------------'
+    print 'No1:' + str(Vector(['8.462','7.893','-8.187']).cross(Vector(['6.984','-5.975','4.778'])))
+    print 'No2:' + str(Vector(['-8.987','-9.838','5.031']).parallelogram(Vector(['-4.268','-1.861','-8.866'])))
+    print 'No3:' + str(Vector(['1.5','9.547','3.691']).triangle(Vector(['-6.007','0.124','5.772'])))
 
 if __name__ == '__main__':
     main()
