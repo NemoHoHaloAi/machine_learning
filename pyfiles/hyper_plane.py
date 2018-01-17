c47107854721a6593015dd01705326db60730b2e
kael <helong9291@sina.com>
2018-01-17 22:15:30 +0800

    new hyper_plane 推广到更高纬度的空间方程式

---
+++ pyfiles/hyper_plane.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+project_name	xxx
+function	xxx
+author		Ho Loong
+date		xxxx-xx-xx
+company		Aispeech,Inc.
+ps              Please be pythonic.
+'''
+
+import sys
+import os
+
+def enviroment_init():
+    """
+    程序运行环境初始化
+    """
+    pass
+
+def main():
+    """
+    程序入口
+    """
+    enviroment_init()
+    pass
+
+if __name__ == '__main__':
+    main()
